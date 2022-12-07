982c8c130192a039c1f48a933216da7f861eadd5
blurryface2809 <grazimashado@gmail.com>
2022-12-06 22:50:55 -0300

    terceira

--- novo-projeto/lojinha/src/components/Filter.js
+++ novo-projeto/lojinha/src/components/Filter.js
@@ -1,17 +1,74 @@
+import { useState } from "react"
 import styled from "styled-components"
+import sun from "../Assets/sun.png"
 
 const Container = styled.div`
   width: 20%;
   height: 100%;
   display: flex;
-  background-color: #f3c4fb;
-  border-right: 4px solid #f7edf0;
+  background-color: #d0f4de;
+  border-right: 5px solid #ffffff;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+  gap: 20px;
+  >div{
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    
+  }
+  h1{
+    display: flex;
+    justify-content: space-between;
+    align-items: start;
+    gap: 10px;
+  }
+  
 `
 
+
+
+
 export function Filter () {
+    
+    const [name, setName] = useState("")
+
+    const onChangeName = (event) => {
+        setName(event.target.value)
+        console.log(event.target.value)
+    }
+
+    const [minPrice, setMinPrice] = useState(Number)
+
+    const [maxPrice, setMaxPrice] = useState(Number)
+
+
     return (
         <Container>
-        <h1>Filter</h1>
+        <h1>Filters <img src={sun} alt="ícone de planetas girando em torno do sol" heigth="25px" width="25px"/></h1>
+        <div>
+            <div>
+                <label>
+                Busca por Nome:
+                </label>
+                <input type="text" value={name} onChange={onChangeName}/>
+            </div>
+            <div>
+                <label>
+                Preço Mínimo:
+                </label>
+                <input type="number" value={minPrice} onChange={(event)=>setMinPrice(event.target.value)}/>
+            </div>
+            <div>
+                <label>
+                Preço Máximo:
+                </label>
+                <input type="number" value={maxPrice} onChange={(event)=>setMaxPrice(event.target.value)}/> 
+            </div>
+            
+        </div>
         </Container>
     )
 }
--- novo-projeto/lojinha/src/components/Footer.js
+++ novo-projeto/lojinha/src/components/Footer.js
@@ -1,4 +1,6 @@
 import styled from "styled-components"
+import github from "../Assets/github.png"
+import linkedin from "../Assets/linkedin.png"
 
 const Container = styled.div`
 width: 100vw;
@@ -7,14 +9,22 @@ display: flex;
 justify-content: space-around;
 align-items: center;
 background-color: #fcf6bd;
-border-top: 4px solid #f7edf0;
+border-top: 5px solid #ffffff;
+h3{
+    display:flex;
+    justify-content: space-around;
+    gap: 10px;
+}
 `
 
 export function Footer () {
     return (
         <Container>
-        <h3>Feito por Graziela Machado (Github e linkedin)</h3>
-        <p>Inpiração: www.ninabrinquedoseducativos.com.br</p>
+        <h3>Feito por Graziela Machado 
+        <a  target="_blank" rel="noreferrer" href="https://github.com/blurryface2809"> <img src={github} alt="ícone do Github" height="20" width="20"/></a>
+        <a  target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/grazimashado/"> <img src={linkedin} alt="ícone do Github" height="20" width="20"/></a>
+        </h3>
+        <p>Inspiração: <a target="_blank" rel="noreferrer" href="https://www.ninabrinquedoseducativos.com.br/">www.ninabrinquedoseducativos.com.br</a></p>
         </Container>
     )
 }
